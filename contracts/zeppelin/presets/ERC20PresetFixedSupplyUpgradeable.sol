442bb1960c6fecad66bd0f3a82bf0ce9fac2620d
pedro <pedro@thinkanddev.com>
2021-04-21 12:49:03 -0300

    Add tests

--- contracts/zeppelin/presets/ERC20PresetFixedSupplyUpgradeable.sol
+++ contracts/zeppelin/presets/ERC20PresetFixedSupplyUpgradeable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.2;
+pragma solidity >=0.6.0 <0.8.0;
 
 import "../token/ERC20/ERC20BurnableUpgradeable.sol";
 import "../proxy/Initializable.sol";
