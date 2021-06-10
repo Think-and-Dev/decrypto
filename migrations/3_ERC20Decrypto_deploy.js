e352010b85f9f23d58c4d39d2dd23b976060f0ed
facu <facundogregori@hotmail.com>
2021-06-10 12:39:41 -0300

    add multiple tokens to deploy

--- migrations/3_ERC20Decrypto_deploy.js
+++ migrations/3_ERC20Decrypto_deploy.js
@@ -8,32 +8,34 @@ module.exports = async function (deployer, network, accounts) {
    const owner = isLocalDeploy ? accounts[0] : deployedJson.Owner;
    const proxyAdminAddr = deployedJson.ProxyAdmin;
 
-   const tokenSymbol = "DTKN";
-   if (isLocalDeploy || !deployedJson[tokenSymbol]) {
-      deployedJson[tokenSymbol] = {};
-      deployedJson[tokenSymbol].Symbol = tokenSymbol;
-      deployedJson[tokenSymbol].TokenName = "Decrypto token";
-      deployedJson[tokenSymbol].Decimals = 18;
-   }
-   const tokenName = deployedJson[tokenSymbol].TokenName;
+   const tokensToDeploy = [{ Symbol: "DTKN", TokenName: "Decrypto token" }, { Symbol: "STKN", TokenName: "S token" }];
 
    await deployer.deploy(ERC20Decrypto);
    const erc20Decrypto = await ERC20Decrypto.deployed();
    //initialize erc20 by owner
    await erc20Decrypto.initialize("", "", owner);
-   deployedJson[tokenSymbol].Logic = erc20Decrypto.address.toLowerCase();
 
-   if (isLocalDeploy || !deployedJson[tokenSymbol].Proxy) {
-      const data = erc20Decrypto.contract.methods.initialize(tokenName, tokenSymbol, owner).encodeABI();
-      await deployer.deploy(TransparentUpgradeableProxy, erc20Decrypto.address, proxyAdminAddr, data);
-      const proxy = await TransparentUpgradeableProxy.deployed();
-      deployedJson[tokenSymbol].Proxy = proxy.address.toLowerCase();
-   } else {
-      const proxy = await Proxy.at(deployedJson[tokenSymbol].Proxy);
-      const proxyAdmin = await ProxyAdmin.at(proxyAdminAddr);
-      proxyAdmin.upgrade(proxy.address, erc20Decrypto.address);
+   for (let index = 0; index < tokensToDeploy.length; index++) {
+      const tokenSymbol = tokensToDeploy[index].Symbol;
+      const tokenName = tokensToDeploy[index].TokenName;
+
+      if (isLocalDeploy || !deployedJson[tokenSymbol] || !deployedJson[tokenSymbol].Proxy) {
+         deployedJson[tokenSymbol] = {};
+         deployedJson[tokenSymbol].Symbol = tokenSymbol;
+         deployedJson[tokenSymbol].TokenName = tokenName;
+         deployedJson[tokenSymbol].Decimals = 18;
+
+         const data = erc20Decrypto.contract.methods.initialize(tokenName, tokenSymbol, owner).encodeABI();
+         await deployer.deploy(TransparentUpgradeableProxy, erc20Decrypto.address, proxyAdminAddr, data);
+         const proxy = await TransparentUpgradeableProxy.deployed();
+         deployedJson[tokenSymbol].Proxy = proxy.address.toLowerCase();
+      } else {
+         const proxy = await Proxy.at(deployedJson[tokenSymbol].Proxy);
+         const proxyAdmin = await ProxyAdmin.at(proxyAdminAddr);
+         proxyAdmin.upgrade(proxy.address, erc20Decrypto.address);
+      }
+      deployedJson[tokenSymbol].Logic = erc20Decrypto.address.toLowerCase();
+
+      deployHelper.saveDeployed(deployedJson);
    }
-
-   deployHelper.saveDeployed(deployedJson);
-
 };
