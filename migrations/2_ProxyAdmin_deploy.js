781b54f423ddd78ca08efbb6af839b793dad2bf2
facu <facundogregori@hotmail.com>
2021-06-10 17:10:15 -0300

    add validate owner

--- migrations/2_ProxyAdmin_deploy.js
+++ migrations/2_ProxyAdmin_deploy.js
@@ -4,6 +4,9 @@ const deployHelper = require("../deployed/deployHelper");
 module.exports = async function (deployer, network, accounts) {
     const deployedJson = deployHelper.getDeployed(network);
     const isLocalDeploy = deployHelper.isLocalNetwork(network)
+    if (!isLocalDeploy && !deployedJson.owner) {
+        throw new Error("Es necesario definir un owner en el json del deploy");
+    }
     const owner = isLocalDeploy ? accounts[0] : deployedJson.owner;
 
     if (isLocalDeploy || !deployedJson.ProxyAdmin) {
